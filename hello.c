d3e87c4aff8f4632beccd9d9d1461452fa220397
alexle27 <alexander.le@huskers.unl.edu>
2020-08-23 19:59:37 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Alex Le
+ * Date: 2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+<<<<<<< HEAD
+  printf("Alexander \n");
+=======
+  printf("Alex \n Computer Science \n");
+>>>>>>> ba36bec2ca0e781490cfb1d104ee080d802298c7
+
+  return 0;
+}
