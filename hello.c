d3105667b9f4e9955a009d6323c11e2e0fa5368a
alexle27 <alexander.le@huskers.unl.edu>
2020-08-23 20:08:10 +0000

    Added a git ignore file

--- hello.c
+++ hello.c
@@ -10,11 +10,7 @@
 
 int main(int argc, char **argv) {
 
-<<<<<<< HEAD
   printf("Alexander \n");
-=======
-  printf("Alex \n Computer Science \n");
->>>>>>> ba36bec2ca0e781490cfb1d104ee080d802298c7
 
   return 0;
 }
